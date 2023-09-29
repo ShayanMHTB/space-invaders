d23dde410f3083f8c4267ffbe26e9de11adaca2c
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-09-29 12:28:21 +0200

    ship class modified for reset after each win/loss

--- ship.py
+++ ship.py
@@ -1,14 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship:
-  """
-    A class to manage the ship.
-  """
+class Ship(Sprite):
+  """A class to manage the ship."""
 
   def __init__(self, ai_game):
-    """
-      Initialize the ship and set its starting position.
-    """
+    """Initialize the ship and set its starting position."""
+    super().__init__()
     self.screen = ai_game.screen
     self.settings = ai_game.settings
     self.screen_rect = ai_game.screen.get_rect()
@@ -28,9 +26,7 @@ class Ship:
     self.moving_left = False
 
   def update(self):
-    """
-      Update the ship's position based on the movement flag.
-    """
+    """Update the ship's position based on the movement flag."""
     # Update the ship's x value, not the rect.
     if self.moving_right and self.rect.right < self.screen_rect.right:
       self.x += self.settings.ship_speed
@@ -41,9 +37,7 @@ class Ship:
     self.rect.x = self.x
 
   def blitme(self):
-    """
-      Draw the ship at its current location.
-    """
+    """Draw the ship at its current location."""
     self.screen.blit(self.image, self.rect)
 
   def center_ship(self):
